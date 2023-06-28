f78999c2fc5bf480f1074edacae5b8c4bac1f240
ly <1663929970@qq.com>
2023-06-28 10:26:47 +0800

    1

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//@ts-ignore
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import router from './router'
 
 const app = createApp(App)
-
 app.use(createPinia())
+app.use(ElementPlus, {
+  locale: zhCn,
+})
 app.use(router)
 
 app.mount('#app')
